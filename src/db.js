103ba7ca0135cf63e6108951835709d5ece25461
Muahmmed Sahal <200~npmuhammedsahal@gmail.com>
2024-11-18 18:38:34 +0530

    Added admin login

--- src/db.js
+++ src/db.js
@@ -1,27 +1,20 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import User from "../models/user.js";
+
 dotenv.config();
 
-const connectDB = async () => {
+ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+        
         console.log("Database connected successfully");
     } catch (error) {
         console.error("Database connection error:", error);
-        process.exit(1); // Exit the application if the connection fails
+        process.exit(1); 
     }
 };
-
-// Define the user schema and model
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    password: { type: String, required: true }
-});
-
-// Using `collection` as the model name
-const collection = mongoose.model('collection', userSchema);
-
-export { connectDB, collection };
+export {connectDB,User as collection} ;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import session from 'express-session';
-import bcrypt from 'bcrypt';
-import dotenv from 'dotenv';
-import { connectDB,collection } from './db.js'; 
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
+import express from "express";
+import session from "express-session";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import { connectDB,collection } from "./db.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 connectDB();
 dotenv.config();
@@ -14,12 +13,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(session({
-    secret: process.env.SESSION_SECRET, 
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } 
-}));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false },
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,8 +30,13 @@ app.set("views", path.join(__dirname, "../views"));
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Route to display the login page
-app.get("/login", (req, res) => {
-    res.render("login", { user_exist: null });
+app.get("/login",async (req, res) => {
+
+    if (req.session.user_name) {
+        res.redirect("/");
+    } else {
+        res.render("login", { user_exist: null });
+    }
 });
 // Route to handle login form submission
 app.post("/login", async (req, res) => {
@@ -51,10 +57,18 @@ app.post("/login", async (req, res) => {
         res.render("login", { user_exist: "Password is not correct." });
     }
 });
-app.get("/signup", (req, res) => {
-    res.render("signup", { error: null });
+// Route to display the signup page
+app.get("/signup",(req, res) => {
+    if (req.session.user_name) {
+        res.redirect("/");
+    } else {
+        res.render("signup", { error: null });
+    }
+    
 });
+// Route to handle signup form submission
 app.post("/signup", async (req, res) => {
+    
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const data = {
         name: req.body.username,
@@ -69,14 +83,74 @@ app.post("/signup", async (req, res) => {
         res.redirect("/login");
     }
 });
+// Route to display the home page
 app.get("/", (req, res) => {
+    req.session.logedin = "loggedin";
     if (req.session.user_name) {
-        console.log("Logged in as:", req.session.user_name); 
-        res.render("home", { user: req.session.user_name });
+        console.log("Logged in as:", req.session.user_name);
+        res.render("home", { user: req.session.user_name, error:null });
+    } else if (req.session.admin_name) {
+        console.log("Logged in as:", req.session.admin_name);
+        res.render("home", { user: req.session.admin_name,error });
     } else {
-        res.redirect('/login'); 
+        res.redirect("/login");
     }
 });
+// Route to logout the user
+app.get("/logout", function (req, res) {
+    if (req.session.admin_name) {
+        req.session.auth = null;
+        res.clearCookie("auth");
+        req.session.destroy(function () {});
+        res.redirect("/admin_login");
+    } 
+    else if(req.session.user_name){
+        req.session.auth = null;
+        res.clearCookie("auth");
+        req.session.destroy(function () {});
+        res.redirect("/");
+    }
+    else {
+        
+        try {
+            res.redirect("/");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+});
+app.get("/admin_login", (req, res) => {
+    if(req.session.admin_name)
+        res.render('dashboard');
+    else{
+        res.render('admin_login', { send: null })
+    }
+});
+app.post("/admin_login", async (req, res) => { 
+    const admin_user = await collection.findOne({ role:1 });
+    console.log(admin_user);
+    
+    const password_match = await bcrypt.compare(
+        req.body.password,
+        admin_user.password
+    );
+    req.session.admin_name = admin_user.name;
+    if (req.body.username === admin_user.name && password_match) {
+        res.redirect("/Dashboard");
+    } else if (!password_match) {
+        res.render("admin_login", { send: "password is not correct" });
+    } else {
+        res.render("admin_login", { send: "Admin can only login" });
+    }
+});
+app.get("/Dashboard", async (req, res) => {
+        const results = await collection.find({ admin: { $exists: false } });
+        if (req.session.admin_name) 
+            res.render("dashboard", { user: results });
+        else {
+            res.redirect("/admin_login");
+        }
+});
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server Started on http://localhost:${port}`);
 });
